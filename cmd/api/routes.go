9b1df23e24e74614e5033161487d7dabd70de018
Kubilay Humm <lieqb2@gmail.com>
2021-10-18 16:55:18 +0300

    custom 404,405 responses added

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,8 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
 	router.HandlerFunc(http.MethodGet, "/v1/api/healthcheck", app.healthCheckHandler)
 
 	return router
